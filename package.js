9851545d2d1512253ba6a8b8f1c6d945d9c881af
Dmytro Les <dmitriy.s.les@gmail.com>
2015-10-26 13:55:39 +0200

    Git link updated

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
     version: '1.2.0',
     summary: 'Polymer lets you build encapsulated, ' +
     're-usable elements to use in building web applications.',
-    git: 'git@github.com:Polyroid/polymer.git'
+    git: 'https://github.com/Polyroid/polymer.git'
 });
 
 Package.onUse(function (api) {
